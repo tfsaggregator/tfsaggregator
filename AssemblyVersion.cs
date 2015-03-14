a2958035db64f75e4d7829bcb10e1a7599d5aaad
Giulio Vian <giuliovdev@gmail.com>
2015-03-14 16:19:56 +0100

    AssemblyInformationalVersion

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -27,3 +27,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0-alfa")]
