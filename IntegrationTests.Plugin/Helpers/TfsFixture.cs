35a70a54f07faca55f022332493b6b1c310daca1
Giulio Vian <giuliovdev@gmail.com>
2015-11-13 00:44:48 +0100

    First two integration tests

--- IntegrationTests.Plugin/Helpers/TfsFixture.cs
+++ IntegrationTests.Plugin/Helpers/TfsFixture.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Win32;
+﻿using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -13,22 +16,48 @@ namespace IntegrationTests.Plugin
 {
     public class TfsFixture : IDisposable
     {
+        // configuration variables
         protected string SQLInstance { get; private set; }
         protected string SQLDataDir { get; private set; }
-        protected string Collection { get; private set; }
-        protected string Project { get; private set; }
-        protected string TfsConfig { get; private set; }
-        protected string TfsServiceControl { get; private set; }
+        protected string CollectionName { get; private set; }
+        protected string ProjectName { get; private set; }
+        // computed values
+        protected string TfsPluginsFolder { get; private set; }
+        protected string TfsConfigExePath { get; private set; }
+        protected string TfsServiceControlExePath { get; private set; }
 
         public TfsFixture()
         {
             this.SQLInstance = string.Format(@"{0}\SQLEXPRESS", Environment.MachineName);
-            this.Collection = "TfsAggregator2IntegrationTest";
-            this.Project = "ScrumTfvc";
+            this.CollectionName = "TfsAggregator2IntegrationTest";
+            this.ProjectName = "ScrumTfvc";
             this.SQLDataDir = @"C:\Program Files\Microsoft SQL Server\MSSQL12.SQLEXPRESS\MSSQL\DATA";
 
             this.DetectTeamFoundationSever();
+            this.DropCollectionDatabase();
+            this.PushAggregator();
             this.RestoreCollectionDatabase();
+
+            var tfsUrl = new Uri("http://localhost:8080/tfs/" + this.CollectionName);
+            this.Collection = new TfsTeamProjectCollection(tfsUrl);
+            this.WorkItemStore = (WorkItemStore)this.Collection.GetService(typeof(WorkItemStore));
+            this.Project = this.WorkItemStore.Projects[this.ProjectName];
+        }
+
+        public TfsTeamProjectCollection Collection { get; private set; }
+        public WorkItemStore WorkItemStore { get; private set; }
+        public Project Project { get; private set; }
+
+        public void PushPolicies(string policyFilename)
+        {
+            File.Copy(
+                string.Format(
+                    @"..\..\ConfigurationsForTests\{0}.policies",
+                    policyFilename),
+                Path.Combine(
+                    this.TfsPluginsFolder,
+                    "TFSAggregator2.ServerPlugin.policies"),
+                true);
         }
 
         protected void DetectTeamFoundationSever()
@@ -42,8 +71,9 @@ namespace IntegrationTests.Plugin
                 using (var tfsKey = tfsBaseKey.OpenSubKey(latestVersionKey))
                 {
                     string tfsInstallPath = tfsKey.GetValue("InstallPath").ToString();
-                    this.TfsConfig = Path.Combine(tfsInstallPath, @"Tools\TfsConfig.exe");
-                    this.TfsServiceControl = Path.Combine(tfsInstallPath, @"Tools\TfsServiceControl.exe");
+                    this.TfsPluginsFolder = Path.Combine(tfsInstallPath, @"Application Tier\Web Services\bin\Plugins");
+                    this.TfsConfigExePath = Path.Combine(tfsInstallPath, @"Tools\TfsConfig.exe");
+                    this.TfsServiceControlExePath = Path.Combine(tfsInstallPath, @"Tools\TfsServiceControl.exe");
                 }
             }
         }
@@ -67,6 +97,7 @@ namespace IntegrationTests.Plugin
             }
         }
 
+        [SuppressMessage("Microsoft.Security", "CA2100")]
         private void ExecSql(string command)
         {
             string connString = string.Format(@"Data Source={0};Initial Catalog=master;Integrated Security=True", this.SQLInstance);
@@ -83,20 +114,33 @@ namespace IntegrationTests.Plugin
             }
         }
 
+        private void DropCollectionDatabase()
+        {
+            Run(this.TfsServiceControlExePath, "unquiesce");
+
+            Run(this.TfsConfigExePath, string.Format("collection /delete /collectionName:{0} /noprompt", this.CollectionName));
+            ExecSql(string.Format(@"ALTER DATABASE [Tfs_{0}] SET  SINGLE_USER WITH ROLLBACK IMMEDIATE; DROP DATABASE [Tfs_{0}]", this.CollectionName));
+
+            File.Delete(Path.Combine(this.SQLDataDir, string.Format("Tfs_{0}.mdf", this.CollectionName)));
+            File.Delete(Path.Combine(this.SQLDataDir, string.Format("Tfs_{0}_log.ldf", this.CollectionName)));
+        }
+
+        private void PushAggregator()
+        {
+            //TODO: for now it is a post-build event
+        }
+
         protected void RestoreCollectionDatabase()
         {
-            Run(this.TfsServiceControl, "unquiesce");
+            ZipFile.ExtractToDirectory(
+                string.Format(
+                    @"..\..\{0}.zip",
+                    this.CollectionName),
+                this.SQLDataDir);
 
-            Run(this.TfsConfig, string.Format("collection /delete /collectionName:{0} /noprompt", this.Collection));
-            ExecSql(string.Format(@"ALTER DATABASE [Tfs_{0}] SET  SINGLE_USER WITH ROLLBACK IMMEDIATE; DROP DATABASE [Tfs_{0}]", this.Collection));
-
-            File.Delete(Path.Combine(this.SQLDataDir, string.Format("Tfs_{0}.mdf", this.Collection)));
-            File.Delete(Path.Combine(this.SQLDataDir, string.Format("Tfs_{0}_log.ldf", this.Collection)));
-            ZipFile.ExtractToDirectory(this.Collection + ".zip", this.SQLDataDir);
-
-            ExecSql(string.Format(@"CREATE DATABASE [Tfs_{0}] ON (FILENAME = N'{1}\Tfs_{0}.mdf'), (FILENAME = N'{1}\Tfs_{0}_log.ldf') FOR ATTACH", this.Collection, this.SQLDataDir));
-            Run(this.TfsServiceControl, "unquiesce");
-            Run(this.TfsConfig, string.Format(@"collection /attach /collectionDb:{1};Tfs_{0} /noprompt", this.Collection, this.SQLInstance));
+            ExecSql(string.Format(@"CREATE DATABASE [Tfs_{0}] ON (FILENAME = N'{1}\Tfs_{0}.mdf'), (FILENAME = N'{1}\Tfs_{0}_log.ldf') FOR ATTACH", this.CollectionName, this.SQLDataDir));
+            Run(this.TfsServiceControlExePath, "unquiesce");
+            Run(this.TfsConfigExePath, string.Format(@"collection /attach /collectionDb:{1};Tfs_{0} /noprompt", this.CollectionName, this.SQLInstance));
         }
 
         #region IDisposable Support
@@ -108,7 +152,8 @@ namespace IntegrationTests.Plugin
             {
                 if (disposing)
                 {
-                    // TODO: dispose managed state (managed objects).
+                    // dispose managed state (managed objects).
+                    this.Collection.Dispose();
                 }
 
                 // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
