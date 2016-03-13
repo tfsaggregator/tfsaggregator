8fd8b43f1166f9baedca4b4d1d98a2429f537549
Jesse Houwing <jesse.houwing@gmail.com>
2016-03-13 18:57:58 +0100

    Use th esupplied access mappign instead of looking it up a second time.

--- Aggregator.Core/Extensions/LocationServiceExtensions.cs
+++ Aggregator.Core/Extensions/LocationServiceExtensions.cs
@@ -10,10 +10,9 @@ namespace Aggregator.Core.Extensions
 {
     public static class LocationServiceExtensions
     {
-        [SuppressMessage("Maintainability", "S1172:Unused method parameters should be removed", Justification = "Required by original interface", Scope = "member", Target = "~M:Aggregator.Core.Extensions.LocationServiceExtensions.GetSelfReferenceUri(Microsoft.VisualStudio.Services.Location.Server.ILocationService,Microsoft.TeamFoundation.Framework.Server.IVssRequestContext,Microsoft.VisualStudio.Services.Location.AccessMapping)~System.Uri")]
         public static Uri GetSelfReferenceUri(this ILocationService self, IVssRequestContext context, AccessMapping mapping)
         {
-            string url = self.GetSelfReferenceUrl(context, self.GetDefaultAccessMapping(context));
+            string url = self.GetSelfReferenceUrl(context, mapping);
             return new Uri(url, UriKind.Absolute);
         }
     }
