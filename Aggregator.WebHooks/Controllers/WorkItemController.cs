53a4dc261eb7cdec84efceacb13372cd8999ad8d
Giulio Vian <giuliovdev@gmail.com>
2016-09-06 21:46:11 +0200

    Split request data from controller logic

--- Aggregator.WebHooks/Controllers/WorkItemController.cs
+++ Aggregator.WebHooks/Controllers/WorkItemController.cs
@@ -2,6 +2,7 @@
 using Aggregator.Core.Context;
 using Aggregator.Core.Monitoring;
 using Aggregator.Models;
+using Aggregator.WebHooks.Models;
 using Aggregator.WebHooks.Utils;
 using Newtonsoft.Json.Linq;
 using System;
@@ -16,91 +17,6 @@ namespace Aggregator.WebHooks.Controllers
 {
     public class WorkItemController : ApiController
     {
-        internal class WorkItemRequest
-        {
-            // pseudo-data
-            internal bool IsValid
-            {
-                get
-                {
-                    return string.IsNullOrWhiteSpace(this.Error);
-                }
-            }
-            internal string Error { get; private set; }
-            // real data
-            internal string EventId { get; private set; }
-            internal string EventType { get; private set; }
-            internal string AccountId { get; private set; }
-            public string CollectionId { get; private set; }
-            internal int WorkItemId { get; private set; }
-            internal string TeamProject { get; private set; }
-            internal string TfsCollectionUri { get; private set; }
-
-            private WorkItemRequest()
-            {
-                this.Error = string.Empty;
-            }
-
-            static internal WorkItemRequest Parse(JObject payload)
-            {
-                var result = new WorkItemRequest();
-
-                if (payload.Property("eventType") == null)
-                {
-                    result.Error = $"Could not determine event type for message: {payload}";
-                }
-                else
-                {
-                    result.EventType = (string)payload["eventType"];
-                    result.EventId = (string)payload["id"];
-
-                    // TODO in the future we will use also the Organization level
-                    if (payload.Property("resourceContainers") == null)
-                    {
-                        // bloody Test button
-                        result.Error = $"Test button generates bad messages: do not use with this service.";
-                    }
-                    else
-                    {
-                        // VSTS sprint 100 or so introduced the Account, but TFS 2015.3 stil lacks it
-                        if (payload.SelectToken("resourceContainers.account") == null)
-                        {
-                            result.CollectionId = (string)payload["resourceContainers"]["collection"]["id"];
-                        }
-                        else
-                        {
-                            result.AccountId = (string)payload["resourceContainers"]["account"]["id"];
-                        }
-                    }
-
-                    result.WorkItemId = (int)payload["resource"]["workItemId"];
-                    string fullUrl = (string)payload["resource"]["url"];
-                    result.TfsCollectionUri = fullUrl.Substring(0, fullUrl.IndexOf("_apis"));
-
-                    switch (result.EventType)
-                    {
-                        case "workitem.created":
-                            result.TeamProject = (string)payload["resource"]["fields"]["System.TeamProject"];
-                            break;
-                        case "workitem.updated":
-                            result.TeamProject = (string)payload["resource"]["revision"]["fields"]["System.TeamProject"];
-                            break;
-                        case "workitem.restored":
-                            result.TeamProject = (string)payload["resource"]["fields"]["System.TeamProject"];
-                            break;
-                        case "workitem.deleted":
-                            result.TeamProject = (string)payload["resource"]["fields"]["System.TeamProject"];
-                            break;
-                        default:
-                            result.Error = $"Unsupported eventType {result.EventType}";
-                            break;
-                    }//switch
-
-                }//if
-                return result;
-            }
-        }
-
         public HttpResponseMessage Get()
         {
             var response = new HttpResponseMessage(HttpStatusCode.OK);
@@ -172,8 +88,8 @@ namespace Aggregator.WebHooks.Controllers
 
         private void Log(string message)
         {
-            //HACK: need something better
             Trace.WriteLine(message);
+            EventLog.WriteEntry("TFSAggregator", message, EventLogEntryType.Warning, 42);
         }
     }
 }
---
+++ Aggregator.WebHooks/Models/WorkItemRequest.cs
@@ -0,0 +1,93 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aggregator.WebHooks.Models
+{
+    internal class WorkItemRequest
+    {
+        // pseudo-data
+        internal bool IsValid
+        {
+            get
+            {
+                return string.IsNullOrWhiteSpace(this.Error);
+            }
+        }
+        internal string Error { get; private set; }
+        // real data
+        internal string EventId { get; private set; }
+        internal string EventType { get; private set; }
+        internal string AccountId { get; private set; }
+        public string CollectionId { get; private set; }
+        internal int WorkItemId { get; private set; }
+        internal string TeamProject { get; private set; }
+        internal string TfsCollectionUri { get; private set; }
+
+        private WorkItemRequest()
+        {
+            this.Error = string.Empty;
+        }
+
+        static internal WorkItemRequest Parse(JObject payload)
+        {
+            var result = new WorkItemRequest();
+
+            if (payload.Property("eventType") == null)
+            {
+                result.Error = $"Could not determine event type for message: {payload}";
+            }
+            else
+            {
+                result.EventType = (string)payload["eventType"];
+                result.EventId = (string)payload["id"];
+
+                // TODO in the future we will use also the Organization level
+                if (payload.Property("resourceContainers") == null)
+                {
+                    // bloody Test button
+                    result.Error = $"Test button generates bad messages: do not use with this service.";
+                }
+                else
+                {
+                    // VSTS sprint 100 or so introduced the Account, but TFS 2015.3 stil lacks it
+                    if (payload.SelectToken("resourceContainers.account") == null)
+                    {
+                        result.CollectionId = (string)payload["resourceContainers"]["collection"]["id"];
+                    }
+                    else
+                    {
+                        result.AccountId = (string)payload["resourceContainers"]["account"]["id"];
+                    }
+                }
+
+                result.WorkItemId = (int)payload["resource"]["workItemId"];
+                string fullUrl = (string)payload["resource"]["url"];
+                result.TfsCollectionUri = fullUrl.Substring(0, fullUrl.IndexOf("_apis"));
+
+                switch (result.EventType)
+                {
+                    case "workitem.created":
+                        result.TeamProject = (string)payload["resource"]["fields"]["System.TeamProject"];
+                        break;
+                    case "workitem.updated":
+                        result.TeamProject = (string)payload["resource"]["revision"]["fields"]["System.TeamProject"];
+                        break;
+                    case "workitem.restored":
+                        result.TeamProject = (string)payload["resource"]["fields"]["System.TeamProject"];
+                        break;
+                    case "workitem.deleted":
+                        result.TeamProject = (string)payload["resource"]["fields"]["System.TeamProject"];
+                        break;
+                    default:
+                        result.Error = $"Unsupported eventType {result.EventType}";
+                        break;
+                }//switch
+
+            }//if
+            return result;
+        }
+    }
+}
