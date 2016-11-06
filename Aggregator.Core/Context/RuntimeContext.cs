9c892e0da2bf7acac2b9e03c05d89d542ce895d4
Jesse Houwing <jesse.houwing@gmail.com>
2016-11-06 11:40:31 -0800

    Fixed a bunch of warnings.

--- Aggregator.Core/Context/RuntimeContext.cs
+++ Aggregator.Core/Context/RuntimeContext.cs
@@ -70,6 +70,7 @@ namespace Aggregator.Core.Context
             }
 
             runtime = runtime.Clone() as RuntimeContext;
+
             // as it changes at each invocation, must be set again here
             runtime.RequestContext = requestContext;
             runtime.workItemRepository = null;
@@ -130,6 +131,7 @@ namespace Aggregator.Core.Context
         public ILogEvents Logger { get; private set; }
 
         private Func<IRuntimeContext, IScriptLibrary> scriptLibraryBuilder;
+
         public ScriptEngine GetEngine()
         {
             const string EngineCacheKey = "engine:";
