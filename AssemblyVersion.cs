60a5d916e1a6802987b30a49bd2e37cb054ee1b6
Giulio Vian <giuliovdev@gmail.com>
2017-07-27 19:38:33 +0200

    Build support for TFS 2017u2
    Fixing references, updated packages, bump version number, some cleanup, update packages target
    Passes full build and unit tests

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -3,7 +3,9 @@
 // To read Configuration use this Powershell snippet
 // [System.Reflection.Assembly]::LoadFile("path-to-assembly-file").GetCustomAttributesData() | ?{ $_.AttributeType -eq [System.Reflection.AssemblyConfigurationAttribute] } | select ConstructorArguments
 #if DEBUG
-#if TFS2017
+#if TFS2017u2
+    [assembly: AssemblyConfiguration("Debug [TFS 2017.2]")]
+#elif TFS2017
     [assembly: AssemblyConfiguration("Debug [TFS 2017.0]")]
 #elif TFS2015u2
     [assembly: AssemblyConfiguration("Debug [TFS 2015.2]")]
@@ -15,7 +17,9 @@
     [assembly: AssemblyConfiguration("Debug [TFS 2013.5]")]
 #endif
 #else
-#if TFS2017
+#if TFS2017u2
+    [assembly: AssemblyConfiguration("Release [TFS 2017.2]")]
+#elif TFS2017
     [assembly: AssemblyConfiguration("Release [TFS 2017.0]")]
 #elif TFS2015u2
     [assembly: AssemblyConfiguration("Release [TFS 2015.2]")]
@@ -29,7 +33,7 @@
 #endif
 [assembly: AssemblyCompany("TFS Aggregator Team")]
 [assembly: AssemblyProduct("TFS Aggregator")]
-[assembly: AssemblyCopyright("Copyright (c) TFS Aggregator Team 2015-16")]
+[assembly: AssemblyCopyright("Copyright (c) TFS Aggregator Team 2015-17")]
 [assembly: AssemblyTrademark("")]
 
 // Version information for an assembly consists of the following four values:
@@ -42,6 +46,6 @@
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
-[assembly: AssemblyInformationalVersion("2.2-alpha")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyInformationalVersion("2.3-alpha")]
