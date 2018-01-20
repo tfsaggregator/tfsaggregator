a152c50bd379d8345d57955a27d65d7a1287e4d0
Jesse Houwing <jesse.houwing@gmail.com>
2018-01-20 17:10:01 +0100

    Update all projects to 2017 project format

--- Aggregator.ConsoleApp/Properties/AssemblyInfo.cs
+++ Aggregator.ConsoleApp/Properties/AssemblyInfo.cs
@@ -4,9 +4,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Aggregator.ConsoleApp")]
-[assembly: AssemblyDescription("Console tool to test TFS Aggregator polices")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
